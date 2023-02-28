852442231f26df4ac7ce9cbc76a98c5e67225af1
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-02-27 19:16:35 -0700

    add add links

--- src/Navigation/Links/index.js
+++ src/Navigation/Links/index.js
@@ -1,11 +1,19 @@
 import HomeIcon from "@mui/icons-material/Home";
 import AboutIcon from "@mui/icons-material/Info";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ContactIcon from "@mui/icons-material/ContactMail";
 export const LinkSection = [
 	{
 		name: "Basic",
 		links: [
 			{ to: "/", text: "Home", Icon: HomeIcon },
 			{ to: "/about", text: "About", Icon: AboutIcon },
+			{ to: "/contact", text: "Contact", Icon: ContactIcon },
+			{
+				to: "/settings",
+				text: "Settings",
+				Icon: SettingsIcon,
+			},
 		],
 	},
 	{
