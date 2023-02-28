055026073db73c8b14c82e057ee53af2c841ab98
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-02-27 18:53:40 -0700

    sign in with google, email/password, create account

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import QueryClientContextProvider from "./Query";
 import ThemeContext from "./Theme";
 import Layout from "./Layout";
 import RouterContext, { Pages } from "./Router";
+import { SnackbarProvider } from "notistack";
 import "./style.css";
 
 export default function App() {
 	return (
 		<HelmetContext>
-			<AuthContext>
-				{/* Query Client must be a child of auth context or fetch will run twice */}
-				<QueryClientContextProvider>
-					<ThemeContext>
-						<RouterContext>
-							<Layout>
-								<Pages />
-							</Layout>
-						</RouterContext>
-					</ThemeContext>
-				</QueryClientContextProvider>
-			</AuthContext>
+			<SnackbarProvider>
+				<AuthContext>
+					{/* Query Client must be a child of auth context or fetch will run twice */}
+					<QueryClientContextProvider>
+						<ThemeContext>
+							<RouterContext>
+								<Layout>
+									<Pages />
+								</Layout>
+							</RouterContext>
+						</ThemeContext>
+					</QueryClientContextProvider>
+				</AuthContext>
+			</SnackbarProvider>
 		</HelmetContext>
 	);
 }
