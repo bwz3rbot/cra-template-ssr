9a16f0943e0d1bf8c76c40647f5ebe8da7b4b7a6
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-03-06 12:16:52 -0700

    rename Loading to LoadingScreen

--- src/Router/Suspend/index.js
+++ src/Router/Suspend/index.js
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import LoadingScreen from "../../Component/Loading";
+import LoadingScreen from "../../Component/LoadingScreen";
 export const Suspend = ({ children }) => {
 	return <Suspense fallback={<LoadingScreen />}>{children}</Suspense>;
 };
