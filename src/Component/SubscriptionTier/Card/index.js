603e78ebfc45156651c6019e67d0f1cc595458d6
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-03-06 23:27:54 -0700

    add $/interval

--- src/Component/SubscriptionTier/Card/index.js
+++ src/Component/SubscriptionTier/Card/index.js
@@ -65,7 +65,7 @@ export default function SubscriptionTierCard({
 						amount,
 						currency,
 					})}
-					{interval}
+					/{interval}
 				</Typography>
 			</CardContent>
 			<CardActionArea
