de7d7f22f94b1a53f3e74fefbe11add2cbe22123
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-03-08 22:56:52 -0700

    debug service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,22 +5,12 @@ importScripts(
 	"https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
 );
 
-// Set Firebase configuration, once available
-self.addEventListener("fetch", () => {
+const getParams = () => {
 	const urlParams = new URLSearchParams(location.search);
-	self.firebaseConfig = Object.fromEntries(urlParams);
-});
-
-// "Default" Firebase configuration (prevents errors)
-const defaultConfig = {
-	apiKey: true,
-	projectId: true,
-	messagingSenderId: true,
-	appId: true,
+	return Object.fromEntries(urlParams);
 };
 
-// Initialize Firebase app
-firebase.initializeApp(self.firebaseConfig || defaultConfig);
+firebase.initializeApp(getParams());
 const messaging = firebase.messaging();
 
 // Configure message handler (assumes backend is set up)
