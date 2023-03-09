d00fdf2dc438558c7be0167cfc0f7558d4be172a
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-03-08 17:59:09 -0700

    firebase auth state

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,20 +1,17 @@
+import { createContext, useContext, useMemo, useState } from "react";
+
+import { FIREBASE_CONFIG } from "./config";
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {
-	getMessaging,
-	getToken,
-	isSupported,
-	onMessage,
-} from "firebase/messaging";
-import { pick } from "lodash";
-import { useSnackbar } from "notistack";
+
+import { getMessaging } from "firebase/messaging";
 
 import {
 	getAuth,
 	signInAnonymously,
 	signInWithPopup,
 	signInWithEmailAndPassword,
-	setPersistence,
 	browserSessionPersistence,
 	GoogleAuthProvider,
 	EmailAuthProvider,
@@ -23,17 +20,15 @@ import {
 	sendEmailVerification,
 	sendPasswordResetEmail,
 } from "firebase/auth";
-
-import LoadingScreen from "../Component/LoadingScreen";
-
-import { createContext, useContext, useEffect, useState } from "react";
-import { FIREBASE_CONFIG } from "./config";
-import { useAuthContext } from "./auth";
+const app = initializeApp(FIREBASE_CONFIG);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
 const Context = createContext({
-	app: null,
-	auth: null,
-	messaging: null,
-	analytics: null,
+	app,
+	auth,
+	messaging,
+	analytics,
 	user: null,
 	isAuthenticated: false,
 	isAnonymous: false,
@@ -57,128 +52,44 @@ const Context = createContext({
 });
 
 export default function FirebaseAppContextProvider({ children }) {
-	const [state, setState] = useState({
-		app: null,
-		auth: null,
-		messaging: null,
-		analytics: null,
-		initialized: false,
-		user: null,
-	});
-
-	useEffect(() => {
-		// this effect will run once on mount and initialize the firebase app
-		if (state.initialized) return;
-		let mounted = true;
-		const asyncEffect = async () => {
-			const app = initializeApp(FIREBASE_CONFIG);
-			const auth = getAuth(app);
-			onAuthStateChanged(auth, user => {
-				// state needs to be given a new reference to auth here
-				// or the currently logged in user it will be out of sync
-
-				setState(state => ({
-					...state,
-					auth,
-				}));
+	const [currentUser, setCurrentUser] = useState(auth.currentUser);
+	useMemo(() => {
+		// handle auth state change - sign in anonymously if no user
+		let unsubscribe = () => {};
+		auth.setPersistence(browserSessionPersistence).then(persistence => {
+			unsubscribe = onAuthStateChanged(auth, user => {
+				if (user) {
+					setCurrentUser(user); // set the currentUser state if one exists
+				} else {
+					signInAnonymously(auth); // otherwise, sign in anonymously
+				}
 			});
-			await setPersistence(auth, browserSessionPersistence);
-
-			const analytics = getAnalytics(app);
-			mounted &&
-				setState({
-					app,
-					auth,
-					analytics,
-					initialized: true,
-				});
-		};
-		asyncEffect();
+		});
 		return () => {
-			mounted = false;
+			unsubscribe();
 		};
-	}, [state]);
-
-	useEffect(() => {
-		// this effect will run once the app has been initialized and will create an anonymous session
-		// if the user creates a new account it will be merged with the anonymous session,
-		// allowing them to retain their user id and any data they have created under the anonymous session
-		if (!state.initialized) return;
-		if (state.auth.currentUser) return;
-		signInAnonymously(state.auth);
-	}, [state]);
-
-	useEffect(() => {
-		// this effect is required to pass idToken to apollo client
-		// it will run whenever a new user logs in or out
-		// getIdToken function is asynchronous, so it must be done in an effect
-		let mounted = true;
-		if (!state.auth) return;
-		if (!state?.auth?.currentUser) {
-			return;
-		}
-		const asyncEffect = async () => {
-			if (state.auth.currentUser && !state.idToken) {
-				const idToken = await state.auth.currentUser?.getIdToken();
-				mounted &&
-					setState(state => ({
-						...state,
-						user: {
-							...state.auth.currentUser,
-							idToken,
-						},
-					}));
-			}
-		};
-		asyncEffect();
-	}, [state?.auth?.currentUser]);
-
-	const isAuthenticated = Boolean(!!state?.user?.idToken);
-	const isAnonymous = state?.user?.isAnonymous || false;
-
-	useEffect(() => {
-		// refresh the token every 55 minutes
-		let mounted = true;
-		if (!state.user) return;
-		const timeInterval = 55 * 60 * 1000; // 55 minutes
-		const interval = setInterval(async () => {
-			const newToken = await state?.auth?.currentUser?.getIdToken(true);
-			mounted &&
-				setState(state => ({
-					...state,
-					user: {
-						...state.user,
-						idToken: newToken,
-					},
-				}));
-		}, timeInterval);
-		return () => {
-			mounted = false;
-			clearInterval(interval);
-		};
-	}, [state?.user?.idToken]);
+	}, [currentUser]);
 
 	return (
 		<Context.Provider
 			value={{
-				app: state.app,
-				auth: state.auth,
-				analytics: state.analytics,
-				user: state.user,
-				isAuthenticated,
-				isAnonymous,
+				app,
+				auth,
+				analytics,
+				messaging,
+				user: currentUser,
+				isAuthenticated: !!currentUser,
+				isAnonymous: currentUser?.isAnonymous || false,
 				username:
-					state.auth?.currentUser?.displayName ||
-					state.auth?.currentUser?.email ||
-					null,
+					currentUser?.displayName || currentUser?.email || null,
 				signInWithEmailAndPassword: ({
 					email,
 					password,
 					onSuccess = () => {},
 					onError = () => {},
 				} = {}) => {
-					if (!state.auth) return;
-					signInWithEmailAndPassword(state.auth, email, password)
+					if (!auth) return;
+					signInWithEmailAndPassword(auth, email, password)
 						.then(onSuccess)
 						.catch(onError);
 				},
@@ -186,18 +97,17 @@ export default function FirebaseAppContextProvider({ children }) {
 					onSuccess = () => {},
 					onError = () => {},
 				} = {}) => {
-					if (!state.auth) return;
+					if (!auth) return;
 					const provider = new GoogleAuthProvider();
 
 					/* popup seems to work better on mobile than using redirect */
-					signInWithPopup(state.auth, provider)
+					signInWithPopup(auth, provider)
 						.then(onSuccess)
 						.catch(onError);
 				},
 				signOut: async () => {
-					if (!state.auth) return;
-					setState(state => ({ ...state, idToken: null }));
-					await state.auth.signOut().catch(err => {
+					if (!auth) return;
+					await auth.signOut().catch(err => {
 						console.error(err);
 					});
 				},
@@ -205,8 +115,8 @@ export default function FirebaseAppContextProvider({ children }) {
 					onSuccess = () => {},
 					onError = () => {},
 				} = {}) => {
-					if (!state.auth) return;
-					sendEmailVerification(state.auth.currentUser)
+					if (!auth) return;
+					sendEmailVerification(auth.currentUser)
 						.then(onSuccess)
 						.catch(onError);
 				},
@@ -214,11 +124,8 @@ export default function FirebaseAppContextProvider({ children }) {
 					onSuccess = () => {},
 					onError = () => {},
 				} = {}) => {
-					if (!state.auth) return;
-					sendPasswordResetEmail(
-						state.auth,
-						state.auth.currentUser.email
-					)
+					if (!auth) return;
+					sendPasswordResetEmail(auth, auth.currentUser.email)
 						.then(onSuccess)
 						.catch(onError);
 				},
@@ -233,25 +140,16 @@ export default function FirebaseAppContextProvider({ children }) {
 					// This causes auth state to update and onAuthStateChanged fire -
 					// causing the app to re-render with a new user context
 
-					if (!state.auth) return;
+					if (!auth) return;
 					try {
-						const currentUser = state.auth.currentUser;
+						const currentUser = auth.currentUser;
 						const Credential = EmailAuthProvider.credential(
 							email,
 							password
 						);
 
 						await linkWithCredential(currentUser, Credential);
-						await signInWithEmailAndPassword(
-							state.auth,
-							email,
-							password
-						);
-						// this is a weird bug where the user is signed in but the auth state is not updated
-						// likely need to re-implement onAuthStateChanged as a proper callback
-						setState(state => ({
-							...state,
-						}));
+						await signInWithEmailAndPassword(auth, email, password);
 
 						onSuccess();
 					} catch (err) {
@@ -259,32 +157,9 @@ export default function FirebaseAppContextProvider({ children }) {
 						return;
 					}
 				},
-				enableMessaging: () => {
-					if (!state.messaging) return;
-					const VAPID_KEY = process.env.REACT_APP_FIREBASE_VAPID_KEY;
-					getToken(state.messaging, { vapidKey: VAPID_KEY });
-				},
-				logToken: async () => {
-					if (!state.messaging) return;
-					const VAPID_KEY = process.env.REACT_APP_FIREBASE_VAPID_KEY;
-					console.log({
-						messaging: state.messaging,
-					});
-					const permission =
-						await state.messaging.requestPermission();
-
-					const res = await getToken(state.messaging, {
-						vapidKey: VAPID_KEY,
-						serviceWorkerRegistration: state.swRegistration,
-					}).catch(err => {
-						console.log("Error getting token");
-						console.error(err);
-					});
-					console.log(res);
-				},
 			}}
 		>
-			{!state.initialized ? <LoadingScreen /> : <>{children}</>}
+			{children}
 		</Context.Provider>
 	);
 }
@@ -292,6 +167,6 @@ export default function FirebaseAppContextProvider({ children }) {
 export const useFirebaseContext = () => {
 	return useContext(Context);
 };
-export { useAuthContext } from "./auth";
+export { useAuthContext } from "./Auth";
 
-export { useMessaging } from "./messaging";
+export { useMessaging } from "./Messaging";
