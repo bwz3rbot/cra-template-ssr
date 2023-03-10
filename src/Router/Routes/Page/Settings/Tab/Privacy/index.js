7f8941aabc2bf0f4e3a364db224a8ee53474eaab
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-03-10 12:23:40 -0700

    move settings panel to reusable component

--- src/Router/Routes/Page/Settings/Tab/Privacy/index.js
+++ src/Router/Routes/Page/Settings/Tab/Privacy/index.js
@@ -1,3 +1,68 @@
+import {
+	Grid,
+	Card,
+	CardContent,
+	FormControlLabel,
+	Switch,
+} from "@material-ui/core";
 export default function SettingsPagePrivacyTab() {
-	return <>Privacy Tab</>;
+	return (
+		<>
+			<Grid container spacing={2}>
+				<Grid item xs={12}>
+					<Card>
+						<CardContent>
+							<Grid container spacing={2}>
+								{settings.map((setting, index) => {
+									return (
+										<Grid item xs={12} key={index}>
+											<FormControlLabel
+												control={
+													<Switch
+														checked={setting.value}
+														name={setting.name}
+														color="primary"
+													/>
+												}
+												label={setting.label}
+												onChange={e => {
+													console.log(
+														"updating config"
+													);
+													updateConfig({
+														variables: {
+															input: {
+																[setting.name]:
+																	e.target
+																		.checked,
+															},
+														},
+														onCompleted: data => {
+															console.log(
+																"updated config",
+																data
+															);
+														},
+
+														refetchQueries: [
+															{
+																query: definitions
+																	.notifications
+																	.query
+																	.config,
+															},
+														],
+													});
+												}}
+											/>
+										</Grid>
+									);
+								})}
+							</Grid>
+						</CardContent>
+					</Card>
+				</Grid>
+			</Grid>
+		</>
+	);
 }
