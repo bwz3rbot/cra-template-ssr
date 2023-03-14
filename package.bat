5d89d3a6a07560d53f37231626530f14787419f5
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-03-14 11:45:00 -0700

    archive script

--- package.bat
+++ package.bat
@@ -1,5 +1,7 @@
-@REM create zip file of the ./template folder
-
-call git archive --format=zip --output=template.zip HEAD
-@REM add the build folder to the zip file
+@REM Build the project
+call npm run build
+@REM Create an archive of the template folder
+call git archive --format=zip --output=deploy.zip HEAD
+@REM use 7z to add folder ./build to the existing zip file
+call 7z a -tzip deploy.zip ./build
 
