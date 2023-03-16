7f4e215be507b4f5962da33414e85afe48c29e2d
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-03-15 23:40:16 -0700

    set babel preset runtime automatic to not require import react in every file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,16 @@ require("ignore-styles");
 
 require("@babel/register")({
 	ignore: [/(node_module)/],
-	presets: ["@babel/preset-env", "@babel/preset-react"],
+	presets: [
+		"@babel/preset-env",
+		[
+			"@babel/preset-react",
+			{
+				// runtime: "automatic" allows us to use server side JSX without importing React in every file
+				runtime: "automatic",
+			},
+		],
+	],
 });
 
 require("./listener");
