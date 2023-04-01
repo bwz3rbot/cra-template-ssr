2d534460fd716a5645c71d8533c3fb9a36504be2
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2023-04-01 14:07:25 -0700

    remove batch files

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:16.3.0-alpine3.11
-# where our app will be located in the image
-RUN mkdir -p /app
-WORKDIR /app
-# move all source code
-COPY . .
-RUN npm install
-CMD [ "npm", "run", "eb:prod" ]
-EXPOSE 3000
--- package.bat
+++
@@ -1,7 +0,0 @@
-@REM Build the project
-call npm run build
-@REM Create an archive of the template folder
-call git archive --format=zip --output=deploy.zip HEAD
-@REM use 7z to add folder ./build to the existing zip file
-call 7z a -tzip deploy.zip ./build
-
