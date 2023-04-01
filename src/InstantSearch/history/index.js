bef0c08408de76c90c6d3a6dd8c8dcb1adefca42
Bwz3r <68409068+bwz3rbot@users.noreply.github.com>
2023-04-01 12:09:41 -0700

    debug instant search

--- src/InstantSearch/history/index.js
+++ src/InstantSearch/history/index.js
@@ -1,48 +1,27 @@
 import { InstantSearch } from "react-instantsearch-dom";
 import { searchClient } from "../algoliasearch";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useState, useRef, useEffect } from "react";
 import { urlToSearchState, searchStateToUrl } from "./util";
 
-const DEBOUNCE_TIME = 700;
 export default function InstantSearchContextProvider({
 	children,
 	resultsState,
 }) {
-	const debouncedSetStateRef = useRef();
 	const navigate = useNavigate();
 	const location = useLocation();
-
-	const defaultSearchState = urlToSearchState(location);
-
-	const [searchState, setSearchState] = useState(defaultSearchState);
 
 	const options = {
 		indexName: process.env.REACT_APP_ALGOLIA_INDEX_NAME,
 		searchClient,
 	};
 	if (resultsState) options.resultsState = resultsState;
-
-	useEffect(() => {
-		const newSearchState = urlToSearchState(location);
-		setSearchState(newSearchState);
-	}, [location]);
-
-	function onSearchStateChange(updatedSearchState) {
-		clearTimeout(debouncedSetStateRef.current);
-
-		debouncedSetStateRef.current = setTimeout(() => {
-			const newState = searchStateToUrl(updatedSearchState);
-			navigate(newState);
-		}, DEBOUNCE_TIME);
-
-		setSearchState(updatedSearchState);
-	}
 	return (
 		<InstantSearch
 			{...options}
-			searchState={searchState}
-			onSearchStateChange={onSearchStateChange}
+			searchState={urlToSearchState(location)}
+			onSearchStateChange={updatedSearchState =>
+				navigate(searchStateToUrl(updatedSearchState))
+			}
 		>
 			{children}
 		</InstantSearch>
